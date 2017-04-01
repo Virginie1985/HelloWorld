63facc62677e772ddd2403b64718da41995759f9
virginie <virginie@pc11.home>
2017-04-01 11:58:17 +0200

    Projet1Vivi

---
+++ Projet1Vivi/src/projet1/Projet1Vivi.java
@@ -0,0 +1,38 @@
+package projet1;
+
+public class Projet1Vivi {
+
+	public static void main(String[] args) {
+
+		int[] monTab;
+		monTab = new int [10];
+		int i;
+		i = 0;
+
+		while (i<10){
+			monTab[i]= i+1;
+			i= i+1;
+		}
+
+
+		i = 0;
+
+
+		monTab [4]=-12;
+		monTab [1]=-2;
+		monTab[9]=-2;
+
+		while (i < 10){
+			if(monTab[i]<=0){
+
+				System.out.println(monTab[i]);
+			}
+
+			i=i+1;
+
+		}
+	}
+
+}
+
+
